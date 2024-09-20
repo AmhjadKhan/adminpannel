023f0082fdc06d5856fe1d443c0b32f5a9155b9f
Amhjadkhan <amjadpogramer@gmail.com>
2024-09-20 16:09:51 +0400

    first commit

---
+++ Backend/admin-pannel/src/api/cetagory/controllers/cetagory.ts
@@ -0,0 +1,7 @@
+/**
+ * cetagory controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::cetagory.cetagory');
---
+++ Backend/admin-pannel/src/api/cetagory/routes/cetagory.ts
@@ -0,0 +1,7 @@
+/**
+ * cetagory router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::cetagory.cetagory');
